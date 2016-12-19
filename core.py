0c1246582a3e506f8dadd9917a34f0e6cff16887
evgfilim1 <evgfilim1@yandex.ru>
2016-12-19 22:49:59 +0500

    Fixed code according to PEP-484 Added 'check_destination' function

--- core.py
+++ core.py
@@ -1,29 +1,39 @@
 import pickle
-from telegram import ChatMember, ParseMode, TelegramError
 from datetime import datetime
+
+from telegram import ChatMember, ParseMode, TelegramError
+from telegram import User, Update, Message, Bot
+
 from config import resetTime, botCREATOR, botTOKEN, logChannel
 
 botID = int(botTOKEN[:botTOKEN.index(":")])
 
 
-def get_name(user) -> str:
+def check_destination(bot_name: str, message_text: str) -> bool:
+    msg = message_text.split()
+    msg = msg[0].split('@')
+    msg.append('')
+    return msg[1] == bot_name
+
+
+def get_name(user: User) -> str:
     if bool(user.username):
         return '@' + user.username
     else:
         return user.first_name
 
 
-def loader(filename: str):
+def loader(filename: str) -> dict:
     with open(filename, 'rb') as ff:
         return pickle.load(ff)
 
 
-def saver(obj, filename: str):
+def saver(obj: dict, filename: str):
     with open(filename, 'wb') as ff:
         pickle.dump(obj, ff, pickle.HIGHEST_PROTOCOL)
 
 
-def load_all():
+def load_all() -> (dict, dict, dict, dict):
     if not __import__("os").path.exists("users.pkl"):
         return {}, {}, {}, {}
     chat_users = loader("users.pkl")
@@ -40,7 +50,7 @@ def save_all(chat_users: dict, spin_name: dict, can_change_spin_name: dict, resu
     saver(results, "results.pkl")
 
 
-def is_user_left(chat_user) -> bool:
+def is_user_left(chat_user: ChatMember) -> bool:
     if chat_user.status == ChatMember.LEFT or \
                     chat_user.status == ChatMember.KICKED:
         return True
@@ -48,8 +58,8 @@ def is_user_left(chat_user) -> bool:
         return False
 
 
-def is_private(chat_id: int, user_id: int=0) -> bool:
-    if user_id != 0:
+def is_private(chat_id: int, user_id: int = None) -> bool:
+    if user_id is not None:
         print("WARNING: 'user_id' is deprecated, it will be removed soon")
     return chat_id > 0
 
@@ -63,7 +73,7 @@ def time_diff() -> float:
     return delta
 
 
-def get_message(update):
+def get_message(update: Update) -> Message:
     if bool(update.edited_message):
         return update.edited_message
     else:
@@ -82,13 +92,13 @@ def can_change_name(can_change_spin_name: dict, chat_id: int, user_id: int) -> b
     return user_id in can_change_spin_name[chat_id] or user_id == botCREATOR
 
 
-def log_to_channel(bot, level: str, text: str):
+def log_to_channel(bot: Bot, level: str, text: str):
     bot.sendMessage(chat_id=logChannel, text="{lvl}:\n{txt}\n\n{time}".format(
         lvl=level, txt=text, time=datetime.now()
     ), parse_mode=ParseMode.MARKDOWN)
 
 
-def announce(bot, chat_users: dict, text: str, md: bool=False):
+def announce(bot: Bot, chat_users: dict, text: str, md: bool = False):
     for chat in chat_users.keys():
         try:
             if md:
@@ -99,14 +109,14 @@ def announce(bot, chat_users: dict, text: str, md: bool=False):
             pass
 
 
-def admins_refresh(can_change_spin_name: dict, bot, chat_id: int):
+def admins_refresh(can_change_spin_name: dict, bot: Bot, chat_id: int):
     admins = bot.getChatAdministrators(chat_id=chat_id)
     can_change_spin_name[chat_id] = []
     for admin in admins:
         can_change_spin_name[chat_id].append(admin.user.id)
 
 
-def fix_md(text):
+def fix_md(text: str) -> str:
     md_symbols = '*_`['
     for symbol in md_symbols:
         try:
--- thebot.py
+++ thebot.py
@@ -1,9 +1,11 @@
+import logging
+
 from telegram import *
 from telegram.ext import *
 from telegram.ext.dispatcher import run_async
-import logging
-import core
+
 import config
+import core
 
 TIME_FORMAT = "%d %b, %H:%M:%S"
 LOG_FORMAT = '%(levelname)-8s [%(asctime)s] %(message)s'
@@ -17,28 +19,28 @@ logging.basicConfig(format=LOG_FORMAT, level=logging.INFO, datefmt=TIME_FORMAT)
 chatUsers, spinName, canChangeSN, results = core.load_all()
 
 
-def handle_error(bot, update, error):
+def handle_error(bot: Bot, update: Update, error):
     core.log_to_channel(bot, "WARNING", "The last update caused error!\n```\n{err}\n```".
                         format(err=error))
 
 
-def reset(bot, job):
+def reset(bot: Bot, job: Job=None):
     results.clear()
     core.log_to_channel(bot, "INFO", "Reset done")
 
 
-def auto_save(bot, job):
+def auto_save(bot: Bot, job: Job):
     core.save_all(chatUsers, spinName, canChangeSN, results)
 
 
-def update_cache(bot, update):
+def update_cache(bot: Bot, update: Update):
     msg = core.get_message(update)
     user = msg.from_user
     if not core.is_private(msg.chat_id, user.id):
         chatUsers[msg.chat_id].update({user.id: core.get_name(user)})
 
 
-def admin_shell(bot, update, args):
+def admin_shell(bot: Bot, update: Update, args: list):
     msg = core.get_message(update)
     if msg.from_user.id == config.botCREATOR:
         try:
@@ -57,7 +59,7 @@ def admin_shell(bot, update, args):
         return
 
 
-def svc_handler(bot, update):
+def svc_handler(bot: Bot, update: Update):
     chat_id = update.message.chat_id
     to_id = update.message.migrate_to_chat_id
     if update.message.group_chat_created or \
@@ -80,12 +82,12 @@ def svc_handler(bot, update):
         results.pop(chat_id)
 
 
-def helper(bot, update):
+def helper(bot: Bot, update: Update):
     bot.sendMessage(chat_id=update.message.chat_id, text=config.helpText, parse_mode=ParseMode.MARKDOWN,
                     reply_to_message_id=update.message.message_id)
 
 
-def admin_refresh(bot, update):
+def admin_refresh(bot: Bot, update: Update):
     if core.is_private(update.message.chat_id, update.message.from_user.id):
         bot.sendMessage(chat_id=update.message.chat_id, text="Я не работаю в ЛС")
         return
@@ -94,13 +96,13 @@ def admin_refresh(bot, update):
                     reply_to_message_id=update.message.message_id)
 
 
-def ping(bot, update):
+def ping(bot: Bot, update: Update):
     bot.sendMessage(chat_id=update.message.chat_id, text="Ping? Pong!",
                     reply_to_message_id=update.message.message_id)
 
 
 @run_async
-def do_the_spin(bot, update):
+def do_the_spin(bot: Bot, update: Update):
     chat_id = update.message.chat_id
     if core.is_private(chat_id, update.message.from_user.id):
         bot.sendMessage(chat_id=update.message.chat_id, text="Я не работаю в ЛС")
@@ -120,7 +122,7 @@ def do_the_spin(bot, update):
             sleep(2)
 
 
-def change_spin_name(bot, update, args):
+def change_spin_name(bot: Bot, update: Update, args: list):
     msg = core.get_message(update)
 
     if core.is_private(msg.chat_id, msg.from_user.id):
@@ -139,7 +141,7 @@ def change_spin_name(bot, update, args):
         return
 
 
-def spin_count(bot, update):
+def spin_count(bot: Bot, update: Update):
     if core.is_private(update.message.chat_id, update.message.from_user.id):
         bot.sendMessage(chat_id=update.message.chat_id, text="Я не работаю в ЛС")
         return
