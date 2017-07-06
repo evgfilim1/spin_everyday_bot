06bfc0fca20a5541c658f868a0329297c4368b93
evgfilim1 <evgfilim1@yandex.ru>
2017-07-06 23:12:50 +0500

    Fixed version string

--- config_example.py
+++ config_example.py
@@ -7,7 +7,7 @@
 # You can find out necessary IDs from @ShowJsonBot
 
 # Version string for bot
-__version__ = '1.5.1-dev'
+__version__ = '1.5.3-dev'
 
 # Repository URL to be shown in /about
 REPO_URL = "https://github.com/evgfilim1/spin_everyday_bot"
